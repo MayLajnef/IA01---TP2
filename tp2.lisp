60e1f79f6734dd9d4a21e070275b0c6164e96314
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2024-11-09 16:42:38 +0100

    Update tp2.lisp

--- tp2.lisp
+++ tp2.lisp
@@ -105,51 +105,129 @@
 |#
 
 ;; 2
+;; Fonction qui applique les effets des actions ou des dialogues
 (defun rules (actor action &optional person cc state)
-;; Fonction qui applique les effets des actions ou des dialogues"
   (case action
     ('kill 
-      ;; Applique l'effet killed sur "person" et
+      ;; Applique l'effet killed sur "person" 
       (apply-effect 'killed person 'nil state)
-      ;; Applique risen et moved sur les personnes mangées si "person" = loup
       (when (equal person 'wolf)
-       (when (equal (get-value 'granny 'place state) 'wolf-belly)
-         (apply-effect 'risen 'granny 't state)
-         (apply-effect 'moved 'granny cc state)
-         (format t "~%Chasseur : \"La grand-mère est sauvée !\""))
-       (when (equal (get-value 'red-riding-hood 'place state) 'wolf-belly)
-         (apply-effect 'risen 'red-riding-hood 't state)
-         (apply-effect 'moved 'red-riding-hood cc state)
-         (format t "~%Chasseur : \"Le petit chaperon rouge est sauvé !\""))
-      (format t "~%Chasseur : \"Le loup ne fera plus de mal à personne !\"")
-      )
-    )
+        (format t "~%*Le chasseur charge son fusil*")
+        (format t "~%Chasseur: \"Cette fois-ci, tu ne feras plus de mal à personne, vilaine bête !\"")
+        (format t "~%*BANG !*")
+        ;; Applique risen et moved sur les personnes mangées si "person" = loup
+        (when (equal (get-value 'granny 'place state) 'wolf-belly)
+          (apply-effect 'risen 'granny 't state)
+          (apply-effect 'moved 'granny cc state)
+          (format t "~%*Le chasseur ouvre le ventre du loup et en sort la grand-mère*")
+          (format t "~%Chasseur: \"Dieu soit loué, elle respire encore !\"")
+          (format t "~%Grand-mère: \"Oh... Que s'est-il passé ?\""))
+        (when (equal (get-value 'red-riding-hood 'place state) 'wolf-belly)
+          (apply-effect 'risen 'red-riding-hood 't state)
+          (apply-effect 'moved 'red-riding-hood cc state)
+          (format t "~%*Le chasseur sort aussi le Petit Chaperon Rouge*")
+          (format t "~%Petit Chaperon Rouge: \"Comme il faisait noir là-dedans !\"")
+          (format t "~%Chasseur: \"Tout est fini maintenant, vous êtes en sécurité.\""))
+    ))
+    
     ('eat 
-      ;; Applique l'effet eaten sur "person" et
-      ;; moved sur "person" dans le ventre du loup 
-
-    )
+      ;; Applique l'effet eaten sur "person" 
+      (apply-effect 'eaten person 'nil state)
+      (cond 
+        ((equal person 'granny)
+         (format t "~%*Le loup frappe à la porte*")
+         (format t "~%Loup: *d'une voix douce* \"C'est votre petite-fille !\"")
+         (format t "~%*La grand-mère ouvre la porte*")
+         (format t "~%Loup: \"GRRRR !\"")
+         (format t "~%*En un bond, le loup se jette sur la grand-mère*"))
+        ((equal person 'red-riding-hood)
+         (format t "~%Loup: \"MAINTENANT, C'EST TON TOUR !\"")
+         (format t "~%*Le loup bondit hors du lit*")
+         (format t "~%Petit Chaperon Rouge: \"AU SECOURS !\"")
+         (format t "~%*Mais il était déjà trop tard...*"))))
+    
     ('move 
       ;; Applique moved de "actor" dans cc
-
-    )
-    ('greet 
-      ;; Applique un dialogue
+      (apply-effect 'moved actor cc state)
+      (cond
+        ((equal actor 'red-riding-hood)
+         (format t "~%*Le Petit Chaperon Rouge sautille gaiement vers ~a*" cc)
+         (format t "~%Petit Chaperon Rouge: *chantonne* \"La la la...\""))
+        ((equal actor 'wolf)
+         (format t "~%*Le loup se faufile silencieusement vers ~a*" cc))
+        ((equal actor 'hunter)
+         (format t "~%*Le chasseur se dirige prudemment vers ~a*" cc))
+        (t (format t "~%~a se déplace vers ~a" actor cc))))
     
-    )
+    ('greet 
+      ;; Applique un dialogue de salutations
+      (cond
+        ((and (equal actor 'red-riding-hood) (equal person 'wolf))
+         (format t "~%Loup: \"Bonjour, ma petite.\"")
+         (format t "~%Petit Chaperon Rouge: \"Bonjour, monsieur le Loup.\""))
+        ((and (equal actor 'red-riding-hood) (equal person 'granny))
+         (format t "~%Petit Chaperon Rouge: \"Bonjour, grand-mère !\""))
+        (t (format t "~%~a: \"Bonjour, ~a.\"" actor person))))
+    
     ('give 
-      ;; Applique un dialogue
-
-    )
+      ;; Applique un dialogue où le petit chaperon rouge donne un gâteau à sa grand mère
+      (cond
+        ((and (equal actor 'red-riding-hood) (equal person 'granny))
+         (format t "~%Petit Chaperon Rouge: \"Tenez grand-mère, maman vous envoie une galette et un petit pot de confiture.\"")
+         (format t "~%Grand-mère: \"Oh, merci ma chérie ! Ta mère est si attentionnée.\""))
+        (t (format t "~%~a: \"Voici un présent pour vous, ~a.\"" actor person))))
+    
     ('tell 
       #| Applique un dialogue (différent selon les scènes)
-      vous pouvez utiliser cc pour les distinguer
-      ou faire confiance à votre meilleur ami ChatGPT ou
-      autre IA générative pour savoir quoi dire |#
+      Vous pouvez utiliser cc pour les distinguer ou faire confiance à votre meilleur ami ChatGPT ou autre IA générative pour savoir quoi dire.
+      Pour ma part, j'ai utilisé  Claude.ai. |#
+      (cond
+        ;; Quand le Petit Chaperon Rouge révèle sa destination au loup (s1 -> s2)
+        ((and (equal actor 'red-riding-hood) (equal person 'wolf))
+         (format t "~%Loup: \"Où vas-tu donc, ma petite ?\"")
+         (format t "~%Petit Chaperon Rouge: \"Je vais voir ma grand-mère qui est malade.\"")
+         (format t "~%Loup: \"Elle habite loin, ta grand-mère ?\"")
+         (format t "~%Petit Chaperon Rouge: \"Oh oui ! C'est la première maison après le moulin, là-bas.\"")
+         (format t "~%Loup: \"Eh bien, j'y vais aussi. Je prends ce chemin-ci, prends celui-là, nous verrons qui sera le premier !\""))
+        
+        ;; Quand le loup trompe la grand-mère (s3)
+        ((and (equal actor 'wolf) (equal person 'granny))
+         (format t "~%*Toc toc toc*")
+         (format t "~%Grand-mère: \"Qui est là ?\"")
+         (format t "~%Loup: *imitant la voix du Petit Chaperon Rouge* \"C'est moi, votre petite-fille !\"")
+         (format t "~%Grand-mère: \"Tire la chevillette, et la bobinette cherra.\""))
+        
+        ;; La fameuse scène de dialogue (s4 -> s5)
+        ((and (equal actor 'wolf) (equal person 'red-riding-hood))
+         (format t "~%Petit Chaperon Rouge: \"Grand-mère, que vous avez de grandes oreilles !\"")
+         (format t "~%Loup: \"C'est pour mieux t'écouter, mon enfant.\"")
+         (format t "~%Petit Chaperon Rouge: \"Grand-mère, que vous avez de grands yeux !\"")
+         (format t "~%Loup: \"C'est pour mieux te voir, mon enfant.\"")
+         (format t "~%Petit Chaperon Rouge: \"Grand-mère, que vous avez de grandes dents !\"")
+         (format t "~%Loup: \"C'EST POUR MIEUX TE MANGER !\""))
+        
+        ;; Quand le chasseur arrive (s6 -> s7)
+        ((and (equal actor 'hunter) (equal cc 'granny-home))
+         (format t "~%*Le chasseur entend des ronflements suspects*")
+         (format t "~%Chasseur: \"Tiens, tiens... La mère-grand ne ronfle pas comme ça d'habitude.\""))
+        
+        ;; Le dénouement heureux (s8 -> outcome)
+        ((and (equal actor 'red-riding-hood) (equal person 'granny))
+         (format t "~%Petit Chaperon Rouge: \"Oh grand-mère, j'ai eu si peur !\"")
+         (format t "~%Grand-mère: \"Nous pouvons remercier le brave chasseur.\"")
+         (format t "~%Chasseur: \"Le plus important est que vous soyez saines et sauves.\"")
+         (format t "~%Petit Chaperon Rouge: \"Je promets de ne plus jamais parler aux inconnus dans les bois !\""))
 
-    )
-  )
-)
+        ;; Quand le loup fait la sieste (s6)
+        ((equal actor 'wolf)
+         (format t "~%*Le loup, le ventre bien rempli, s'allonge sur le lit*")
+         (format t "~%Loup: \"Aaaah... Une petite sieste me fera du bien... *RONFLE*\""))
+
+        ;; Dialogue par défaut
+        (t (format t "~%*Une conversation s'engage entre ~a et ~a*" actor person))))
+    
+    (t (format t "~%Erreur : l'action ~a est inconnue." action))))
+
 (rules 'hunter 'kill 'wolf 'granny-home *state*)
 *state*
 #| TESTS
